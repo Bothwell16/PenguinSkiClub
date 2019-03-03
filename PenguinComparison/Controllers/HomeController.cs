1e87ee657e6bae5c54ac13fcb2a1fd5dfb70b176
appextent@gmail.com <gLATERalegator05>
2019-03-03 13:10:48 -0500

    added to todo message to put app purpose in about box

--- PenguinComparison/Controllers/HomeController.cs
+++ PenguinComparison/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace PenguinComparison.Controllers
 
         public ActionResult About()
         {
-            //TODO : update about box with my contact info
+            //TODO : update about box with my contact info and app purpose
             ViewBag.Message = "Your application description page.";
 
             return View();
